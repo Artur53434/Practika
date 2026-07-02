4a8a91ac9ef429c01709f8759452cac6db119334
Artur53434 <Drimas.ter.98@bk.ru>
2026-07-02 17:32:00 +0300

    Всё раскидано по папкам

---
+++ GUI/data_base.py
@@ -0,0 +1,17 @@
+"""
+Поля CSV:
+Дата - ДД.ММ.ГГГГ,
+Время - XX:XX,
+Текст,
+Результат теста в процентах
+"""
+
+import csv
+
+class DataBase:
+    def __init__(self, file_name = "data_base.csv"):
+
+        pass
+
+
+    pass
